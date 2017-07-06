6d3f91295fb1e4c24a0b4a578255b67b5926f5de
kartoffeltoby <kontakt@t-haber.de>
2017-07-06 10:53:05 +0200

    [TASK] add blank multiuser function

--- start.sh
+++ start.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-pass="$PASSWORD"
-user="$USER"
-adduser --quiet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-login --shell /bin/false --home /home/$user/ $user -q
-echo $pass $user
-echo -e "$pass\n$pass" | smbpasswd -s -a $user
-mkdir /home/$user
-/etc/init.d/samba start
-tail -f /var/log/dmesg
+if [ -n "$USER" ]
+then
+ cat /conf/multiuser.conf | while read LINE
+ do
+  value=$(echo $LINE | tr ':' "\n")
+  pass=${value[1]}
+  user=${value[0]}
+  adduser --quiet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-login --shell /bin/false --home /home/$user/ $user -q
+  echo $pass $user
+  echo -e "$pass\n$pass" | smbpasswd -s -a $user
+  mkdir /home/$user
+ done
+ /etc/init.d/samba start
+ tail -f /var/log/dmesg
+else
+ pass="$PASSWORD"
+ user="$USER"
+ adduser --quiet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-login --shell /bin/false --home /home/$user/ $user -q
+ echo $pass $user
+ echo -e "$pass\n$pass" | smbpasswd -s -a $user
+ mkdir /home/$user
+ /etc/init.d/samba start
+ tail -f /var/log/dmesg
+fi
