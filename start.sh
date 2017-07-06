0232767405e5cebbfdd57508af1ea438de05c2f0
kartoffeltoby <kontakt@t-haber.de>
2017-07-06 10:59:42 +0200

    [TASK] set shared public folder

--- start.sh
+++ start.sh
@@ -12,6 +12,12 @@ then
   echo -e "$pass\n$pass" | smbpasswd -s -a $user
   mkdir /home/$user
  done
+ pass="nopublic"
+ user="publicuser"
+ adduser --quiet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-login --shell /bin/false --home /home/$user/ $user -q
+ echo $pass $user
+ echo -e "$pass\n$pass" | smbpasswd -s -a $user
+ mkdir /home/$user
  /etc/init.d/samba start
  tail -f /var/log/dmesg
 else
