77e1ba56cf8282bdbc0abff79da665fffdaddb49
KartoffelToby <kontakt@t-haber.de>
2018-01-10 14:40:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,13 +14,8 @@ then
   echo -e "$pass\n$pass" | smbpasswd -s -a $user
   mkdir -p /home/$user
  done
- pass="nopublic"
- user="publicuser"
- adduser --quiet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-login --shell /bin/false --home /home/$user/ $user -q
- echo $pass $user
- echo -e "$pass\n$pass" | smbpasswd -s -a $user
- mkdir -p /home/$user
- /usr/sbin/smbd -F
+/etc/init.d/samba start
+tail -f /var/log/dmesg
 else
  pass="$PASSWORD"
  user="$USER"
